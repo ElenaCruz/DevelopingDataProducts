3ef7dc2282cb787fa87b6f7b8d7302bcb6c065ff
System Administrator <root@romulo.local>
2015-09-26 20:19:50 +0200

    Completed app

--- server.R
+++ server.R
@@ -1,9 +1,8 @@
 library(UsingR)
 data(galton)
 
-childrenHeight <- function(Parent1Input,Parent2Input) mean(Parent1Input, PArent2Input)
-shinyServer( function(input, output) {
-  output$inputValue1 <- renderPrint({input$Parent1Input})
-  output$inputValue2 <- renderPrint({input$Parent2Input})
-  output$prediction <- renderPrint({childrenHeight(input$Parent1Input, input$Parent2Input)}) }
-  
+childrenHeight <- function(Parent1Input,Parent2Input) mean(c(Parent1Input, Parent2Input))
+shinyServer(function(input, output){
+  output$Parent1Input<- renderPrint({input$Parent1Input})
+  output$Parent2Input <- renderPrint({input$Parent2Input})
+  output$prediction <- renderPrint({childrenHeight(input$Parent1Input,input$Parent2Input)})})
--- ui.R
+++ ui.R
@@ -1,16 +1,17 @@
 shinyUI( pageWithSidebar(
   # Application title 
   headerPanel("Children's height prediction"),
-  h3("We will predict the heigth of the child, based on the heigth of the parent using the Galton\'s dataset"),
   sidebarPanel(
+    h5('We will predict the height of the children considering the height of the parents. Please input the height values.'),
     numericInput('Parent1Input', 'Height for parent 1 (cms)', 170, min = 140, max = 220, step = 1),
     numericInput('Parent2Input', 'Height for parent 2 (cms)', 170, min = 140, max = 220, step = 1), 
-    submitButton('Submit')
-  ), mainPanel(
+    submitButton('Submit')),
+  mainPanel(
     h3('Results of prediction for the height of the children:'),
-    h4("You\'ve entered: "), 
-    verbatimTextOutput(inputValue1), h4("as height for parent 1"),
-    verbatimTextOutput(inputValue2), h4("as height for parent 2")
-#     h4('Which resulted in a prediction of '), verbatimTextOutput("prediction"), h4("as expected height in cm for the child")
+     h4("You\'ve entered the following value for the first parent's height: "),
+     verbatimTextOutput('Parent1Input'), 
+     h4("You\'ve entered the following value for the second parent's height: "),
+     verbatimTextOutput('Parent2Input'),
+     h4('Which resulted in a prediction of '), verbatimTextOutput("prediction"), h4("as expected height in cm for the child")
   ) )
 )
